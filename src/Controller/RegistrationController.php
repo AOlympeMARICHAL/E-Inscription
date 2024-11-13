dc57e903f00d0948c2d9eb789775572e40b8f77d
WACHE Nathan <WACHE@lan.sio.lyceefulbert.fr>
2024-11-13 16:56:27 +0100

    Initial commit

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+/*
+class RegistrationController extends AbstractController
+{
+    #[Route('/registration', name: 'app_registration')]
+    public function index(): Response
+    {
+        return $this->render('registration/index.html.twig', [
+            'controller_name' => 'RegistrationController',
+        ]);
+    }
+}*/
+class RegistrationController extends AbstractController
+{
+    public function index(UserPasswordHasherInterface $passwordHasher): Response
+    {
+        // ... e.g. get the user data from a registration form
+        $user = new User('username', 'email@example.com');
+        $plaintextPassword = 'user_password';
+
+        // hash the password (based on the security.yaml config for the $user class)
+        $hashedPassword = $passwordHasher->hashPassword(
+            $user,
+            $plaintextPassword
+        );
+        $user->setPassword($hashedPassword);
+
+        // ...
+    }
+}
---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+namespace App\Controller\SecurityController;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Bundle\SecurityBundle\Security;
+
+class SecurityController extends AbstractController
+{
+    #[Route(path: '/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+}
+class SecurityController
+{
+    public function someAction(Security $security): Response
+    {
+        // logout the user in on the current firewall
+        $response = $security->logout();
+
+        // you can also disable the csrf logout
+        $response = $security->logout(false);
+
+        // ... return $response (if set) or e.g. redirect to the homepage
+    }
+}
