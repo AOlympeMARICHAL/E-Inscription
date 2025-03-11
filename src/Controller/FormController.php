b44ed1494c89a376da6966ab0d8ba1008a40afd3
AOlympeMARICHAL <olympe.marichal@gmail.com>
2025-03-11 11:41:57 +0100

    template incripUnV2 fini mais pas adapté

--- src/Controller/FormController.php
+++ src/Controller/FormController.php
@@ -25,22 +25,20 @@ class FormController extends AbstractController
     #[Route('/inscription', name: 'app_inscription')]
     public function index(): Response
     {
-        $individu = new Individu;
-        $individuForm = $this->createForm(IndividuType::class, $individu);
-        $eleve = new Eleve;
-        $eleveForm = $this->createForm(EleveType::class, $eleve);
-
-        return $this->render('inscription/un.html.twig', array(
-            'individuForm' => $individuForm,
-            'eleveForm' => $eleveForm
-        ));
+        return $this->render('inscription/un.html.twig');
     }
 
     #[Route("/inscription/getPage1", name: "app_inscription_getPage1")]
     public function page1(Request $request, EntityManagerInterface $em)
     {
-        
+        $individu = new Individu;
+        $individuForm = $this->createForm(IndividuType::class, $individu);
+        $eleve = new Eleve;
+        $eleveForm = $this->createForm(EleveType::class, $eleve);
     
-        return $this->render('inscription/deux.html.twig');
+        return $this->render('inscription/deux.html.twig', array(
+            'individuForm' => $individuForm,
+            'eleveForm' => $eleveForm
+        ));
     }
 }
