156012a0ad023aaa8213fc0c04523800a7879d55
AOlympeMARICHAL <olympe.marichal@gmail.com>
2025-03-11 11:24:57 +0100

    template incripUn fini mais pas adapté

--- src/Controller/FormController.php
+++ src/Controller/FormController.php
@@ -2,6 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Eleve;
+use App\Entity\Individu;
+use App\Form\AContacterType;
+use App\Form\AntecedentType;
+use App\Form\EleveType;
+use App\Form\FinancierType;
+use App\Form\IndividuType;
+use App\Form\ResponsableType;
+use App\Form\UrgenceType;
+use App\Form\UserType;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -9,17 +19,28 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+
 class FormController extends AbstractController
 {
     #[Route('/inscription', name: 'app_inscription')]
     public function index(): Response
     {
-        return $this->render('inscription/un.html.twig');
+        $individu = new Individu;
+        $individuForm = $this->createForm(IndividuType::class, $individu);
+        $eleve = new Eleve;
+        $eleveForm = $this->createForm(EleveType::class, $eleve);
+
+        return $this->render('inscription/un.html.twig', array(
+            'individuForm' => $individuForm,
+            'eleveForm' => $eleveForm
+        ));
     }
 
     #[Route("/inscription/getPage1", name: "app_inscription_getPage1")]
     public function page1(Request $request, EntityManagerInterface $em)
     {
-        // $form = $this->createForm();
+        
+    
+        return $this->render('inscription/deux.html.twig');
     }
 }
