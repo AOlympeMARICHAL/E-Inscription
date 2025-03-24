c48501d053ff450eb48c935a4ec64b23f804066c
Olympe MARICHAL <160247275+AOlympeMARICHAL@users.noreply.github.com>
2025-03-24 17:47:23 +0100

    continu de form (#14)

--- src/Form/EleveType.php
+++ src/Form/EleveType.php
@@ -6,6 +6,9 @@ use App\Entity\Eleve;
 use App\Entity\Financier;
 use App\Entity\Individu;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -20,7 +23,35 @@ class EleveType extends AbstractType
             ->add('dateEntree', null, [
                 'widget' => 'single_text',
             ])
-            ->add('sexe')
+            ->add('sexe', ChoiceType::class, [
+                'label' => ' ',
+                'expanded' => true,
+                'multiple' => false,
+                'choices' => [
+                    'Féminin' => 'F',
+                    'Masculin' => 'M'
+                ],
+                'attr' => [
+                    'class' => 'fr-radio-rich',
+                ],
+                'choice_attr' => function ($choice, $key, $value) {
+                    return [
+                        'style' => 'display: none;',
+                    ];
+                }
+
+            ])
+            ->add('redoublement', ChoiceType::class, [
+                'label' => ' ',
+                'expanded' => true, 
+                'multiple' => true,
+                'choices' => [
+                    'Redoublement' => true
+                ],
+                'attr' => [
+                'class' => 'fr-checkbox-group',
+                ]
+            ])
             ->add('nationality')
             ->add('dateBirth', null, [
                 'widget' => 'single_text',
@@ -28,9 +59,27 @@ class EleveType extends AbstractType
             ->add('departmentBirth')
             ->add('gradeRepetition')
             ->add('regime')
-            ->add('transport')
+            ->add('transport', ChoiceType::class, [
+                'label' => ' ',
+                'expanded' => true, 
+                'multiple' => true,
+                'choices' => [
+                    'Filibus' => 'filibus',
+                    'SNCF' => 'sncf',
+                    'Autres' => 'autres',
+                ],
+                'attr' => [
+                'class' => 'fr-checkbox-group',
+                ]
+
+                ])
             ->add('immaVehicle')
-            ->add('speciality')
+            ->add('speciality', TextType::class, [
+                'label' => ' ',
+                'attr' => [
+                    'class' => 'fr-input'
+                ],
+            ])
             ->add('lv1')
             ->add('lv2')
             ->add('mdl')
@@ -39,7 +88,12 @@ class EleveType extends AbstractType
             ->add('vitalCard')
             ->add('idCertificate')
             ->add('assurance')
-            ->add('cours')
+            ->add('cours', TextType::class, [
+                'label' => ' ',
+                'attr' => [
+                    'class' => 'fr-input'
+                ],
+            ])
             ->add('financier', EntityType::class, [
                 'class' => Financier::class,
                 'choice_label' => 'id',
--- src/Form/IndividuType.php
+++ src/Form/IndividuType.php
@@ -7,7 +7,10 @@ use App\Entity\Individu;
 use App\Entity\Responsable;
 use App\Entity\Urgence;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,14 +19,30 @@ class IndividuType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('lastname')
-            ->add('email')
-            ->add('mobilePhone')
-            ->add('address')
-            ->add('zip')
-            ->add('city')
-            ->add('commune')
+            ->add('name', TextType::class, [
+                'label'=> ' '
+            ])
+            ->add('lastname', TextType::class, [
+                'label'=> ' '
+            ])
+            ->add('email', EmailType::class, [
+                'label' => ' ',
+            ])
+            ->add('mobilePhone', TelType::class,[
+                'label'=> ' '
+            ])
+            ->add('address', TextType::class, [
+                'label' => ' ',
+            ])
+            ->add('zip', TextType::class, [
+                'label' => ' ',
+            ])
+            ->add('city', TextType::class, [
+                'label' => ' ',
+            ])
+            ->add('commune', TextType::class, [
+                'label' => ' ',
+            ])
             ->add('nameBoss')
             ->add('addressBoss')
             ->add('financier', EntityType::class, [
