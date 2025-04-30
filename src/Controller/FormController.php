267c967f37ed7722276ff5cf368f496209b27daf
AOlympeMARICHAL <olympe.marichal@gmail.com>
2025-04-30 15:12:58 +0200

    aaaaaa

--- src/Controller/FormController.php
+++ src/Controller/FormController.php
@@ -26,26 +26,36 @@ class FormController extends AbstractController
     #[Route('/inscription', name: 'app_inscription')]
     public function page1(Request $request, EntityManagerInterface $em): Response
     {
-    
-        // On crée un formulaire composite avec les deux entités
+        // Création des entités
+        $individu = new Individu();
+        $eleve = new Eleve();
+
+
+        // Création d'un formulaire composite avec les entités Individu et Eleve
         $form = $this->createFormBuilder()
             ->add('individu', IndividuType::class)
             ->add('eleve', EleveType::class)
             ->getForm();
-    
+
         $form->handleRequest($request);
-    
+
         if ($form->isSubmitted() && $form->isValid()) {
             $data = $form->getData();
-            
-            $em->persist($data['individu']); 
+
+            // Persistance des données dans la base
+            $em->persist($data['individu']);
+            $data['eleve']->setIndividu($data['individu']);
             $em->persist($data['eleve']);
             $em->flush();
-    
-            // Redirection vers la page deux.html.twig si succès
-            return $this->redirect('app_inscription_antecedent');
+
+            $request->getSession()->set('eleve_id', $data['eleve']->getId());
+
+
+            // Redirection vers la route de la page suivante
+            return $this->redirectToRoute('app_inscription_antecedent');
         }
-    
+
+        // Rendu du formulaire pour la première page
         return $this->render('inscription/un.html.twig', [
             'form' => $form->createView(),
         ]);
@@ -54,7 +64,13 @@ class FormController extends AbstractController
     #[Route('/inscription/antecedent', name: 'app_inscription_antecedent')]
     public function page2(Request $request, EntityManagerInterface $em): Response
     {
-    
+        // Récupération de l'ID de l'élève depuis la session
+        $eleveId = $request->getSession()->get('eleve_id');
+        $eleve = $em->getRepository(Eleve::class)->find($eleveId);
+
+        $antecedent1 = new Antecedent();
+        $antecedent2 = new Antecedent();
+
         // On crée un formulaire composite avec les deux entités
         $form = $this->createFormBuilder()
             ->add('antecedent1', AntecedentType::class)
@@ -66,6 +82,9 @@ class FormController extends AbstractController
     
         if ($form->isSubmitted() && $form->isValid()) {
             $data = $form->getData();
+
+            $data['antecedent1']->setEleve($eleve);
+            $data['antecedent2']->setEleve($eleve);
             
             $em->persist($data['antecedent1']); 
             $em->persist($data['antecedent2']);    
